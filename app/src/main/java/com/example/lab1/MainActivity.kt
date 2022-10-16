618bead9b0b568ee88c59b7806e323d551caa986
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-10-16 14:36:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        val textField = binding.text
+        val inputText = binding.editText
+
+        viewModel.text.observe(this) { textField.text = it.toString() }
+
+        val button = binding.buttonChange
+        button.setOnClickListener {
+            viewModel.initText(inputText.text.toString())
+            inputText.setText("")
+        }
+    }
+}
